12964daf9911d0ff0868056242a1de45ef9b7118
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-12-05 20:25:12 +0100

    Change script for video background

--- js/video.js
+++ js/video.js
@@ -1,5 +1,9 @@
-document
-    .querySelector("#background-video")
-    .querySelector("source").src = `https://cdn.jsdelivr.net/gh/bartekl1/MeteoHomepage/videos/${
-    Math.floor(Math.random() * 7) + 1
-}.mp4`;
+function setVideoBackground() {
+    document.querySelector(
+        "#background-video"
+    ).src = `https://cdn.jsdelivr.net/gh/bartekl1/MeteoHomepage/videos/${
+        Math.floor(Math.random() * 7) + 1
+    }.mp4`;
+}
+
+setTimeout(setVideoBackground, 100);
